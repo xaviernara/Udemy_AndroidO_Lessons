c662849f6eeccaa270cdff6566529d3fc5fc8e77
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2018-05-18 21:08:55 -0700

    Still refreshing memory
    going over adding/editting text and buttons

---
+++ Adding & Editting Text & Buttons/app/src/main/java/com/example/xaviernara/exampleapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.xaviernara.exampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //when you make a botton and you want something to happen when the botton is clicked you must create a method
+    // and send it a "View" variable to complete the action from the view class
+    public void clickFucntion(View view){
+        Log.i("Info","Button Pressed!"); //with this line of code a message in log will display when a  button is clicked that calls this function
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
