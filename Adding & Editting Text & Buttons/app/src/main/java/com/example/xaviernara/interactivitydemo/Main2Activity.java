10f3440e9b7e460d0c3a0ad8e3d0eb61a8941c29
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2018-05-21 17:44:48 -0700

    Toast Demo
    testing how to display messages (toasts) on the app screen  and also refresh previous taught subjects

--- Adding & Editting Text & Buttons/app/src/main/java/com/example/xaviernara/interactivitydemo/Main2Activity.java
+++ Adding & Editting Text & Buttons/app/src/main/java/com/example/xaviernara/interactivitydemo/Main2Activity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.xaviernara.exampleapp.R;
 
 public class Main2Activity extends AppCompatActivity {
 
     public void clickFunction(View view){
+        /*
+
         //this line of code will create a editText variable so you get the input data from the text field
         EditText userNameEditText = (EditText) findViewById(R.id.userNameEditText);
         //this line of code will Log the input to the text field (ie display the username entered)
@@ -25,7 +28,25 @@ public class Main2Activity extends AppCompatActivity {
         //this line of code will log when the log in button is pressed
         Log.i("Button Info","Log In is pressed");
 
+        //this line of code will display a message to the app screen
+        //Toast refers to the act the message pop ups like toast from toaster
+        Toast.makeText(this, "Hey Listen", Toast.LENGTH_SHORT).show();
 
+
+        */
+        //Toast Demo (display the name entered into the textfield as a toast pop up)
+
+        //this line of code will create a editText variable so you get the input data from the text field
+        EditText userNameEditText = (EditText) findViewById(R.id.userNameEditText);
+        //this line of code will Log the input to the text field (ie display the username entered)
+        Log.i("Your Name",userNameEditText.getText().toString());
+
+        //this line of code will log when the log in button is pressed
+        Log.i("Button Info","Log In is pressed");
+
+        //this line of code will display a message to the app screen
+        //Toast refers to the act the message pop ups like toast from toaster
+        Toast.makeText(this, "Hey "+userNameEditText.getText().toString(), Toast.LENGTH_LONG).show();
     }
 
     @Override
