41e9236d82d71ac0316be3034f1e0fc44258c1fc
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2020-07-19 23:39:29 -0700

    adding 2nd activity and pastor bio to 2nd activity
    need to add pastor picture to 2nd activity
    
    need to add func where list choice goes to a specfic activity

--- NewMindKingdomApp/app/src/main/java/com/example/xaviernara/newmindapp/MainActivity.java
+++ NewMindKingdomApp/app/src/main/java/com/example/xaviernara/newmindapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.xaviernara.newmindapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,31 +14,110 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ListView activityListView;
+
+    /*
+    //onClick method for previous button that would switch the screen to the next activity
+    public void nextActivity(View view){
+
+        //Intent is for what you WANT to do when you move to the another activity
+        //Intent intent = new Intent(application Context, class you want to move to);
+        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+
+        //this moves the screen to the activity that the intent above says to
+        //this basically stacks the activities on top of each other
+        //(ie when the back button on the users device the screen will cycle back and forth thru the activities screens)
+        //**THIS IS MANDATORY WHEN YOUR MOVING TO THE NEXT ACTIVITY IF ITS USED IN THE FIRST ACTIVITY
+        startActivity(intent);
+
+        //this passes things to the class that activity that the intent tells it to
+        //intent.putExtra("name of the variable",value of the variable);
+        //Ex:  intent.putExtra("age",78); your sending a int to the another activity
+        intent.putExtra("age",29);
+    }
+    */
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView list = findViewById(R.id.list);
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Pastor Biography");
-        arrayList.add("Previous Messages");
-        arrayList.add("Sign Up");
-        arrayList.add("Website");
-        arrayList.add("Church Location");
-        arrayList.add("Ways to give");
-        arrayList.add("Crest Definition");
+        activityListView = findViewById(R.id.church_list);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,                   android.R.layout.simple_list_item_1, arrayList);
+        final Intent intent = new Intent(getApplicationContext(),PastorBiography.class);
+        //startActivity(intent);
+
+
+
+        final ListView list = findViewById(R.id.church_list);
+        final ArrayList<String> churchList = new ArrayList<>();
+        churchList.add("Meet Prophet Frederick E. Ward Sr.");
+        churchList.add("Previous Messages");
+        churchList.add("Sign Up");
+        churchList.add("Website");
+        churchList.add("Church Location");
+        churchList.add("Ways to give");
+        churchList.add("Our Crest");
+        churchList.add("Church Location");
+        churchList.add("Vision Statement ");
+        churchList.add("Mission Statement");
+        churchList.add("Our 7Rs Concept (Our Mandate)");
+        churchList.add("Pathway to Purpose");
+        churchList.add("What We Believe");
+        churchList.add("Our Culture (I.C.C.E)");
+
+
+        //a ArrayAdapter communicates with a array/arraylist to get the contents of the array/arraylist and also how it will be displayed on the screen
+        // the setAdapter places the contents in the array/arraylist  into the listview variable so it can be displayed (i.e.to list it on the screen)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, churchList);
+
+
+        activityListView.setAdapter(arrayAdapter);
+
+
+        //remember to have something happen when a list item is clicked then you need this line of code
+        activityListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            //this lines of code will log the name of the family memeber thats listed on the screen when clicked
+            //and create a toast when a list item is clicked
+            //AdapterView<?> parent = a type of adapter in the <gridLayout,string, etc...>
+            //View view= itmes being viewed on screen
+            //int position = position of the items on the list (for example: positions of the items in the array )
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Family Name: ",churchList.get(position));
+                //Toast.makeText(this,names.get(position),Toast.LENGTH_LONG)
+                //Toast.makeText(getApplicationContext(), "Hello "+names.get(position),Toast.LENGTH_LONG).show();
+
+                intent.putExtra("churchList", churchList.get(position));
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+        /*
         list.setAdapter(arrayAdapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String clickedItem=(String) list.getItemAtPosition(position);
                 Toast.makeText(MainActivity.this,clickedItem,Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
+
 
 
 
---
+++ NewMindKingdomApp/app/src/main/java/com/example/xaviernara/newmindapp/PastorBiography.java
@@ -0,0 +1,55 @@
+package com.example.xaviernara.newmindapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class PastorBiography extends AppCompatActivity {
+
+
+    //onClick method for previous button that would switch the screen to the next activity
+    public void firstActivity(View view){
+
+
+        //Intent is for what you WANT to do when you move to the another activity
+        //Intent intent = new Intent(application Context, class you want to move to);
+        //Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+
+        //this moves the screen to the activity that the intent above says to
+        //this basically stacks the activities on top of each other
+        //(ie when the back button on the users device the screen will cycle back and forth thru the activities screens)
+        //startActivity(intent);
+
+
+        //this lets the user exit the app when the back button is pushed on the android device
+        //instead of cycling back and forth thru the activities screens like the intent/startActivity way
+        //but if the button is pressed then the activity will still switch back and forth like normal
+        finish();
+    }
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pastor_biography);
+
+        //to get the info you passed using the putExtra() method in the main activity you have to
+        //create another intent in the class you want to use the info in
+        Intent intent = getIntent();
+
+        //intent.getIntExtra(exact name of the variable in the other class you want to pass,default value in case the info isnt sent over);
+        //the info you sent has to be got by using the getExtra that pertains to the info sent over
+        //Ex: if i send a double i have to use intent.getDoubleExtra()
+
+        String listChoice = intent.getStringExtra("churchList");
+
+        Toast.makeText(this,"Hello "+listChoice, Toast.LENGTH_SHORT).show();
+
+
+    }
+}
