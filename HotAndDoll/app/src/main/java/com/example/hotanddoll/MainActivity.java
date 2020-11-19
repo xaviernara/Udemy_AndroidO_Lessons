c15ee75f4b3c5159e7bae43d7ba987a31345d80e
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2020-11-18 22:10:28 -0800

    started hotanddoll app
    made spalsh screen
    making home screen next w/nav bar

---
+++ HotAndDoll/app/src/main/java/com/example/hotanddoll/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.hotanddoll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //https://www.geeksforgeeks.org/android-creating-a-splash-screen/
+
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    //#After completion of 2000 ms, the next activity will get started.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(MainActivity.this,
+                        Home.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(intent);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
