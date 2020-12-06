4894dc9d2b5fe0fa12a9eeaaefcc6ddfed5b5669
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2020-12-05 21:19:09 -0800

    fixed side UI, about to add onclicks for sides

--- HotAndDoll/app/src/main/java/com/example/hotanddoll/Sides.java
+++ HotAndDoll/app/src/main/java/com/example/hotanddoll/Sides.java
@@ -3,12 +3,23 @@ package com.example.hotanddoll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Sides extends AppCompatActivity {
+
+    int sideCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sides);
     }
+
+    public void addSide(View view){
+
+        switch ()
+
+
+    }
+
 }
