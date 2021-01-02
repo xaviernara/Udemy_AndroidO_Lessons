0191376ee850ddc92700a5d4bac8446f7969da56
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2021-01-02 10:16:05 -0800

    starting framelayout example

---
+++ NewMindKingdomApp/app/src/main/java/com/example/xaviernara/newmindapp/Fragment.java
@@ -0,0 +1,23 @@
+package com.example.xaviernara.newmindapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment extends androidx.fragment.app.Fragment {
+
+
+
+    @Override
+    public View onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //setContentView(R.layout.activity_fragment);
+
+        return inflater.inflate(R.layout.activity_fragment,container,false);
+    }
+}
